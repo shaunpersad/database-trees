38c331ea5c1adff8b9820b46d8f00459a96818c3
Shaun Persad <shaunpersad@gmail.com>
2014-04-06 21:58:52 -0400

    removed template comments

--- Node.php
+++ Node.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * Node class file.
- *
- * PHP Version 5.3
- *
- * @package  TopHat
- * @author   Shaun Persad <shaunpersad@gmail.com>
- * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link     http://shaunpersad.com
- */
-
 class Node {
 
     public $id;
--- Tree.php
+++ Tree.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * Tree class file.
- *
- * PHP Version 5.3
- *
- * @package  TopHat
- * @author   Shaun Persad <shaunpersad@gmail.com>
- * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link     http://shaunpersad.com
- */
-
 class Tree {
 
 
